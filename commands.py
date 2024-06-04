31f3825d5a561397ff7834f0c31f391bf8ab637e
Ares164286 <52861672+Ares164286@users.noreply.github.com>
2024-06-04 14:53:43 +0900

    helpをファイルわけ

--- commands.py
+++ commands.py
@@ -4,6 +4,7 @@ import func.hantei as hantei
 import func.jobs as jobs
 import func.stats_6e as stats_6e
 import func.stats_7e as stats_7e
+import func.help as help_cmd
 
 commands_dict = {
     "時間割": call.send_timetable,
@@ -25,37 +26,8 @@ commands_dict = {
     "判定": hantei.hantei,
     "能力値": stats_6e.send_stats,
     "能力値7": stats_7e.send_stats_7e,
-    "help": send_help,
-    "へるぷ": send_help,
-    "ヘルプ": send_help,
+    "help": help_cmd.send_help,
+    "へるぷ": help_cmd.send_help,
+    "ヘルプ": help_cmd.send_help,
 }
-
-command_descriptions = {
-    "時間割": "時間割の画像を送信します。",
-    "伝助": "伝助のリンクを送信します。",
-    "lms": "明星LMSのリンクを送信します。",
-    "べんてん": "勉天のリンクを送信します。",
-    "勉天": "勉天のリンクを送信します。",
-    "ココフォリア": "COCOFOLIAのリンクを送信します。",
-    "職業基本": "基本的な職業データを表示します。",
-    "職業2010": "2010年の職業データを表示します。",
-    "職業ランダム": "基本的な職業データからランダムに1つ表示します。",
-    "職業2010ランダム": "2010年の職業データからランダムに1つ表示します。",
-    "フュージョン": "サーバー内のランダムな2名の表示名を合体させます。",
-    "起きろ": "指定したユーザーに10回のメンションを飛ばします。",
-    "判定": "任意の文章に対して◎◯△✕で返答します。",
-    "能力値": "クトゥルフ6版の能力値を生成します。",
-    "能力値7": "クトゥルフ7版の能力値を生成します。",
-    "活動報告": "活動報告のフォーマットを表示します。",
-    "湯婆婆": "指定したユーザーの名前を変更します。",
-    "起きろ": "指定したユーザーに10回メンションを送信します。",
-    "活動報告": "活動報告を作成します。",
-    
-}
-
-async def send_help(ctx):
-    help_message = "以下は利用可能なコマンドとその説明です:\n\n"
-    for command, description in command_descriptions.items():
-        help_message += f"/{command} - {description}\n"
-    await ctx.send(f"```\n{help_message}\n```")
 
