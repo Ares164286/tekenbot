4246d8374fe0865da7ddd58793f81bfb2112335b
Ares164286 <52861672+Ares164286@users.noreply.github.com>
2024-06-04 15:15:05 +0900

    引数で回数指定

--- func/stats_7e.py
+++ func/stats_7e.py
@@ -1,40 +1,41 @@
 import discord
 import random
 
-async def send_stats_7e(ctx, *args):
-    stats = {
-        "STR": random.randint(15, 90) // 5 * 5,
-        "CON": random.randint(15, 90) // 5 * 5,
-        "POW": random.randint(15, 90) // 5 * 5,
-        "DEX": random.randint(15, 90) // 5 * 5,
-        "APP": random.randint(15, 90) // 5 * 5,
-        "SIZ": (random.randint(40, 90) // 5 * 5) + 30,
-        "INT": (random.randint(40, 90) // 5 * 5) + 30,
-        "EDU": (random.randint(50, 99) // 5 * 5) + 30
-    }
-    hp = (stats["CON"] + stats["SIZ"]) // 10
-    mp = stats["POW"] // 5
-    san = stats["POW"]
-    ide = stats["INT"]
-    luk = stats["POW"]
-    kno = stats["EDU"]
-    db = (stats["STR"] + stats["SIZ"]) // 10
+async def send_stats_7e(ctx, num: int = 1):
+    for _ in range(num):
+        stats = {
+            "STR": random.randint(15, 90) // 5 * 5,
+            "CON": random.randint(15, 90) // 5 * 5,
+            "POW": random.randint(15, 90) // 5 * 5,
+            "DEX": random.randint(15, 90) // 5 * 5,
+            "APP": random.randint(15, 90) // 5 * 5,
+            "SIZ": (random.randint(40, 90) // 5 * 5) + 30,
+            "INT": (random.randint(40, 90) // 5 * 5) + 30,
+            "EDU": (random.randint(50, 99) // 5 * 5) + 30
+        }
+        hp = (stats["CON"] + stats["SIZ"]) // 10
+        mp = stats["POW"] // 5
+        san = stats["POW"]
+        ide = stats["INT"]
+        luk = stats["POW"]
+        kno = stats["EDU"]
+        db = (stats["STR"] + stats["SIZ"]) // 10
 
-    if db <= 8:
-        db = "-1d6"
-    elif db <= 12:
-        db = "-1d4"
-    elif db <= 16:
-        db = "0"
-    elif db <= 24:
-        db = "+1d4"
-    else:
-        db = "+1d6"
+        if db <= 8:
+            db = "-1d6"
+        elif db <= 12:
+            db = "-1d4"
+        elif db <= 16:
+            db = "0"
+        elif db <= 24:
+            db = "+1d4"
+        else:
+            db = "+1d6"
 
-    result = (
-        f"STR:{stats['STR']} CON:{stats['CON']} POW:{stats['POW']} "
-        f"DEX:{stats['DEX']} APP:{stats['APP']} SIZ:{stats['SIZ']} "
-        f"INT:{stats['INT']} EDU:{stats['EDU']}\n"
-        f"[HP:{hp} MP:{mp} SAN:{san} IDE:{ide} LUK:{luk} KNO:{kno} DB:{db}]"
-    )
-    await ctx.send(result)
+        result = (
+            f"STR:{stats['STR']} CON:{stats['CON']} POW:{stats['POW']} "
+            f"DEX:{stats['DEX']} APP:{stats['APP']} SIZ:{stats['SIZ']} "
+            f"INT:{stats['INT']} EDU:{stats['EDU']}\n"
+            f"[HP:{hp} MP:{mp} SAN:{san} IDE:{ide} LUK:{luk} KNO:{kno} DB:{db}]"
+        )
+        await ctx.send(result)
