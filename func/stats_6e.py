8cc5e203f4fe2c88b96b1a30a2ed2c876caa7bb4
Ares164286 <52861672+Ares164286@users.noreply.github.com>
2024-06-04 15:30:12 +0900

    数値引数を受け取れるように

--- func/stats_6e.py
+++ func/stats_6e.py
@@ -1,7 +1,16 @@
 import discord
 import random
 
-async def send_stats(ctx, num: int = 1):
+async def send_stats(ctx, *args):
+    num = 1  # デフォルトの生成回数
+    if args:
+        try:
+            num = int(args[0])
+        except ValueError:
+            await ctx.send("回数は整数で指定してください。")
+            return
+
+    results = []
     for _ in range(num):
         stats = {
             "STR": random.randint(3, 18),
@@ -13,29 +22,30 @@ async def send_stats(ctx, num: int = 1):
             "INT": random.randint(8, 18),
             "EDU": random.randint(6, 21)
         }
-    hp = (stats["CON"] + stats["SIZ"]) // 2
-    mp = stats["POW"]
-    san = stats["POW"] * 5
-    ide = stats["INT"] * 5
-    luk = stats["POW"] * 5
-    kno = stats["EDU"] * 5
-    db = stats["STR"] + stats["SIZ"]
+        hp = (stats["CON"] + stats["SIZ"]) // 2
+        mp = stats["POW"]
+        san = stats["POW"] * 5
+        ide = stats["INT"] * 5
+        luk = stats["POW"] * 5
+        kno = stats["EDU"] * 5
+        db = stats["STR"] + stats["SIZ"]
 
-    if db <= 12:
-        db = "-1d6"
-    elif db <= 16:
-        db = "-1d4"
-    elif db <= 24:
-        db = "0"
-    elif db <= 32:
-        db = "+1d4"
-    else:
-        db = "+1d6"
+        if db <= 12:
+            db = "-1d6"
+        elif db <= 16:
+            db = "-1d4"
+        elif db <= 24:
+            db = "0"
+        elif db <= 32:
+            db = "+1d4"
+        else:
+            db = "+1d6"
 
-    result = (
-        f"STR:{stats['STR']} CON:{stats['CON']} POW:{stats['POW']} "
-        f"DEX:{stats['DEX']} APP:{stats['APP']} SIZ:{stats['SIZ']} "
-        f"INT:{stats['INT']} EDU:{stats['EDU']}\n"
-        f"[HP:{hp} MP:{mp} SAN:{san} IDE:{ide} LUK:{luk} KNO:{kno} DB:{db}]"
-    )
-    await ctx.send(result)
+        result = (
+            f"STR:{stats['STR']} CON:{stats['CON']} POW:{stats['POW']} "
+            f"DEX:{stats['DEX']} APP:{stats['APP']} SIZ:{stats['SIZ']} "
+            f"INT:{stats['INT']} EDU:{stats['EDU']}\n"
+            f"[HP:{hp} MP:{mp} SAN:{san} IDE:{ide} LUK:{luk} KNO:{kno} DB:{db}]"
+        )
+        results.append(result)
+    await ctx.send("\n\n".join(results))
