80a05d3e75bdcf8959f88929a8ba167e0ed39f55
Ares164286 <52861672+Ares164286@users.noreply.github.com>
2024-07-03 17:25:56 +0900

    Update commandsadmin.py

--- commandsadmin.py
+++ commandsadmin.py
@@ -2,7 +2,7 @@ import func.save_messages as save_messages
 import func.helpadmin as help_admin  # helpadmin.pyからインポート
 
 commands_admin_dict = {
-    "履歴を保存": save_messages.save_history_cmd,
+    "履歴を保存": save_messages.SaveMessages.save_history_cmd,
     "管理者ヘルプ": help_admin.send_admin_help,
     # 他の管理者用コマンドをここに追加
 }
