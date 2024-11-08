66decc27edc521ea91c437304469e8bbc5f31675
Ares164286 <52861672+Ares164286@users.noreply.github.com>
2024-11-08 18:44:11 +0900

    Delete .pythonlibs/bin directory

--- .pythonlibs/bin/ldeo_magic2.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-import re
-import sys
-from programs.conversion_scripts2.ldeo_magic2 import main
-if __name__ == "__main__":
-    sys.argv[0] = re.sub(r"(-script\.pyw|\.exe)?$", "", sys.argv[0])
-    sys.exit(main())
